571004a8d98118f3f273d090793dd3cd09aa900f
roma <drakyla60@gmail.com>
2024-01-31 00:48:01 +0200

    fix Chat

--- client/src/components/chat/ChatComponent.js
+++ client/src/components/chat/ChatComponent.js
@@ -1,81 +1,154 @@
-// ChatComponent.js
-import React, { useState } from 'react';
+import {useEffect, useState} from 'react';
 import ChatWindow from './ChatWindow';
-import { Button, Avatar } from '@mui/material';
+import {Avatar} from '@mui/material';
 import CancelIcon from '@mui/icons-material/Cancel';
+import useChat from "../../hooks/useChat";
+import storage from "../../utils/storage";
+import {USER_KEY} from "constants.js";
+import {nanoid} from "nanoid";
 import './ChatComponent.css';
 
 const ChatComponent = () => {
-    const [isChatOpen, setChatOpen] = useState(false);
-    const [chats, setChats] = useState([
-        { id: 1, user: 'КМ' },
-        { id: 2, user: 'РВ' },
-        { id: 2, user: 'РВ' }
-    ]);
+  const {users} = useChat()
+  const userMap = new Map();
 
-    const [cancelIconsVisible, setCancelIconsVisible] = useState([false, false]);
+  const filteredUsers = users.filter((user) => {
+    const alreadyExists = userMap.has(user.userId);
+    if (!alreadyExists) {
+      userMap.set(user.userId, true);
+    }
+    return !alreadyExists;
+  });
 
-    const handleChatToggle = () => {
-        setChatOpen(!isChatOpen);
-    };
+  const [isChatOpen, setChatOpen] = useState(false);
+  const [chats, setChats] = useState([
+    {id: 1, user: 'КМ'},
+    {id: 2, user: 'РВ'},
+  ]);
 
-    const handleMouseEnter = (index) => {
-        setCancelIconsVisible((prev) => {
-            const updatedArray = [...prev];
-            updatedArray[index] = true;
-            return updatedArray;
-        });
-    };
+  const [cancelIconsVisible, setCancelIconsVisible] = useState([false, false]);
 
-    const handleMouseLeave = (index) => {
-        setCancelIconsVisible((prev) => {
-            const updatedArray = [...prev];
-            updatedArray[index] = false;
-            return updatedArray;
-        });
-    };
+  const handleChatToggle = () => {
+    setChatOpen(!isChatOpen);
+  };
 
-    const handleRemoveChat = (event, index) => {
-        // Зупиняємо подальшу передачу подій кліку вище по ієрархії
-        event.stopPropagation();
+  const handleMouseEnter = (index) => {
+    setCancelIconsVisible((prev) => {
+      const updatedArray = [...prev];
+      updatedArray[index] = true;
+      return updatedArray;
+    });
+  };
 
-        setChats((prevChats) => {
-            // Створюємо новий масив чатів, виключаючи чат, який потрібно видалити
-            const updatedChats = prevChats.filter((_, i) => i !== index);
-            return updatedChats;
-        });
-    };
+  const handleMouseLeave = (index) => {
+    setCancelIconsVisible((prev) => {
+      const updatedArray = [...prev];
+      updatedArray[index] = false;
+      return updatedArray;
+    });
+  };
 
-    return (
-        <div className="chat-container">
-            {chats.map((chat, index) => (
-                <div
-                    key={chat.id}
-                    className={`chat-button ${isChatOpen ? 'chat-button-open' : ''}`}
-                    onClick={handleChatToggle}
-                    onMouseEnter={() => handleMouseEnter(index)}
-                    onMouseLeave={() => handleMouseLeave(index)}
-                    style={{ marginBottom: '3px', position: 'relative' }}
-                >
-                    <Avatar>
-                        {/* Використовуйте дані користувача чату, наприклад, chat.user */}
-                        {chat.user}
-                    </Avatar>
-                    {cancelIconsVisible[index] && (
-                        <CancelIcon
-                            className="cancel-icon"
-                            style={{ position: 'absolute', top: -2, right: -2 }}
-                            onClick={(event) => {
-                                event.stopPropagation();
-                                handleRemoveChat(event, index);
-                            }}
-                        />
-                    )}
-                </div>
-            ))}
-            {isChatOpen && <ChatWindow onClose={() => setChatOpen(false)} />}
+  const handleRemoveChat = (event, index) => {
+    event.stopPropagation();
+    setChats((prevChats) => {
+      return prevChats.filter((_, i) => i !== index);
+    });
+  };
+
+  const [formData, setFormData] = useState({
+    userName: '',
+    roomId: 'main_room'
+  })
+
+  const [submitDisabled, setSubmitDisabled] = useState(true)
+
+  useEffect(() => {
+    const isSomeFieldEmpty = Object.values(formData).some((v) => !v.trim())
+    setSubmitDisabled(isSomeFieldEmpty)
+  }, [formData])
+
+  const onChange = ({target: {name, value}}) => {
+    setFormData({...formData, [name]: value})
+  }
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+    if (submitDisabled) return
+
+    const userId = nanoid()
+
+    storage.set(USER_KEY, {
+      userId,
+      userName: formData.userName,
+      roomId: formData.roomId
+    })
+
+    window.location.reload()
+  }
+  const user = storage.get(USER_KEY)
+
+  return (
+    <div className="chat-container">
+      {user ? (
+        chats.map((chat, index) => (
+          <div
+            key={chat.id}
+            className={`chat-button ${isChatOpen ? 'chat-button-open' : ''}`}
+            onClick={handleChatToggle}
+            onMouseEnter={() => handleMouseEnter(index)}
+            onMouseLeave={() => handleMouseLeave(index)}
+            style={{marginBottom: '3px', position: 'relative'}}
+          >
+            <Avatar>{chat.user}</Avatar>
+            {cancelIconsVisible[index] && (
+              <CancelIcon
+                className="cancel-icon"
+                style={{position: 'absolute', top: -2, right: -2}}
+                onClick={(event) => {
+                  event.stopPropagation();
+                  handleRemoveChat(event, index);
+                }}
+              />
+            )}
+          </div>
+        ))
+      ) : (
+        <div className='container name-input'>
+          <h2>Welcome</h2>
+          <form onSubmit={onSubmit} className='form name-room'>
+            <div>
+              <label htmlFor='userName'>Enter your name</label>
+              <input
+                type='text'
+                id='userName'
+                name='userName'
+                minLength={2}
+                required
+                value={formData.userName}
+                onChange={onChange}
+              />
+            </div>
+            <div className='visually-hidden'>
+              <label htmlFor='roomId'>Enter room ID</label>
+              <input
+                type='text'
+                id='roomId'
+                name='roomId'
+                minLength={4}
+                required
+                value={formData.roomId}
+                onChange={onChange}
+              />
+            </div>
+            <button disabled={submitDisabled} className='btn chat'>
+              Chat
+            </button>
+          </form>
         </div>
-    );
+      )}
+      {isChatOpen && <ChatWindow onClose={() => setChatOpen(false)}/>}
+    </div>
+  );
 };
 
 export default ChatComponent;
--- client/src/components/chat/ChatWindow.js
+++ client/src/components/chat/ChatWindow.js
@@ -1,86 +1,67 @@
-import React, { useState } from 'react';
-import { Box, Paper, Input, Button } from '@mui/material';
-import SendIcon from '@mui/icons-material/Send';
-import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import {useState} from 'react';
+import {Box, Paper, Button} from '@mui/material';
+import useChat from "../../hooks/useChat";
+import MessageList from "../Room/MessageList/MessageList";
+import MessageInput from "../Room/MessageInput/MessageInput";
 
-const ChatWindow = ({ onClose }) => {
-    const [messages, setMessages] = useState([
-        { text: 'Привіт, як справи?', sender: 'user' },
-        { text: 'Привіт, все гаразд!', sender: 'other' },
-    ]);
+const ChatWindow = ({onClose}) => {
+  const [width, setWidth] = useState(350);
+  const [height, setHeight] = useState(550);
 
-    const [newMessage, setNewMessage] = useState('');
+  const handleResize = (e, direction, ref, d) => {
+    if (ref && ref.style) {
+      setWidth(ref.style.width);
+      setHeight(ref.style.height);
+    }
+  };
 
-    const handleSendMessage = () => {
-        if (newMessage.trim() === '') return;
-        setMessages([...messages, { text: newMessage, sender: 'user' }]);
-        setNewMessage('');
-    };
+  const {users, messages, log, sendMessage, removeMessage} = useChat();
 
-    const handleKeyDown = (e) => {
-        if (e.key === 'Enter' && !e.shiftKey) {
-            e.preventDefault();
-            handleSendMessage();
-        }
-    };
-
-    return (
-        <Paper elevation={3} sx={{ position: 'fixed', bottom: 10, right: 70, width: 300, height: 400, padding: 2, display: 'flex', flexDirection: 'column' }}>
-            <Box sx={{ backgroundColor: 'primary.main', color: 'white', paddingLeft: 2, display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
-                <p>Месенджер</p>
-                <Button onClick={onClose} variant="contained" sx={{
-                    backgroundColor: 'primary.main',
-                    color: 'white',
-                    minWidth: 'fit-content',
-                    padding: '8px 12px',
-                    // borderRadius: 1,
-                    width: '15%',
-                    marginRight: 1
-                }}>Х</Button>
-            </Box>
-            <Box sx={{ flexGrow: 1, display: 'flex', flexDirection: 'column-reverse', overflowY: 'auto', gap: 1, marginBottom: 1 }}>
-                {messages.slice().reverse().map((message, index) => (
-                    <Paper key={index} elevation={1} sx={{ padding: 1, borderRadius: 1, maxWidth: '80%', alignSelf: message.sender === 'user' ? 'flex-end' : 'flex-start', backgroundColor: message.sender === 'user' ? 'primary.main' : 'background.paper', color: message.sender === 'user' ? 'white' : 'text.primary', display: 'flex', alignItems: 'center', marginBottom: 1 }}>
-                        {message.sender === 'other' && <AccountCircleIcon sx={{ marginRight: 1 }} />}
-                        {message.text}
-                        {message.sender === 'user' && <AccountCircleIcon sx={{ marginLeft: 1 }} />}
-                    </Paper>
-                ))}
-            </Box>
-            <Box sx={{ display: 'flex', alignItems: 'center', marginBottom: 0 }}>
-                <Input
-                    value={newMessage}
-                    onChange={(e) => setNewMessage(e.target.value)}
-                    onKeyDown={handleKeyDown}
-                    placeholder="Напишіть повідомлення"
-                    sx={{
-                        flexGrow: 1,
-                        marginRight: 1,
-                        padding: 1,
-                        backgroundColor: 'white', // Змінено на білий фон
-                        border: '1px solid primary.main',
-                        borderRadius: 1,
-                        color: 'black', // Змінено на чорний колір тексту
-                        width: '85%',
-                    }}
-                />
-                <Button
-                    variant="contained"
-                    onClick={handleSendMessage}
-                    sx={{
-                        backgroundColor: 'primary.main',
-                        color: 'white',
-                        minWidth: 'fit-content',
-                        padding: '8px 12px',
-                        borderRadius: 1,
-                        width: '15%',
-                    }}
-                >
-                    <SendIcon sx={{ marginLeft: 1 }} />
-                </Button>
-            </Box>
-        </Paper>
-    );
+  return (
+    <Paper
+      elevation={4}
+      sx={{
+        width,
+        height,
+        position: 'fixed',
+        bottom: 10,
+        right: 70,
+        padding: 1,
+        display: 'flex',
+        flexDirection: 'column',
+        resize: 'both',
+        overflow: 'auto'
+      }}
+      onResize={handleResize}
+    >
+      <Box sx={{
+        backgroundColor: 'primary.main',
+        color: 'white',
+        paddingLeft: 2,
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center'
+      }}>
+        <p>Месенджер</p> <p>{log}</p>
+        <Button onClick={onClose} variant="contained" sx={{
+          backgroundColor: 'primary.main',
+          color: 'white',
+          minWidth: 'fit-content',
+          padding: '8px 12px',
+          width: '15%',
+          marginRight: 1
+        }}>Х</Button>
+      </Box>
+      <Box sx={{flexGrow: 1, display: 'flex', flexDirection: 'column-reverse', overflowY: 'auto', gap: 1, marginBottom: 1}}>
+        <MessageList
+          log={log}
+          messages={messages}
+          removeMessage={removeMessage}
+        />
+      </Box>
+      <MessageInput sendMessage={sendMessage}/>
+    </Paper>
+  );
 };
 
 export default ChatWindow;
