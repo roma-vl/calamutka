7a1dc9ce8ffdfa8fbce4eea0e2c6b3f234a7e816
roma <drakyla60@gmail.com>
2024-01-31 00:38:27 +0200

    seeed user

--- api/seeds/users.js
+++ api/seeds/users.js
@@ -1,17 +1,34 @@
-const { faker } = require('@faker-js/faker');
-
-exports.seed = function (knex) {
-    // Deletes ALL existing entries
+import { faker } from '@faker-js/faker';
+export const seed = function (knex) {
     return knex('users').del()
         .then(function () {
-            // Inserts seed entries
             const users = [];
-            for (let i = 0; i < 3; i++) {
+            for (let i = 0; i < 5; i++) {
                 users.push({
+                    first_name: faker.person.firstName(),
+                    last_name: faker.person.lastName(),
                     username: faker.internet.userName(),
-                    password: faker.internet.password()
+                    password: faker.internet.password(),
+                    email: faker.internet.email(),
+                    address: faker.location.streetAddress(),
+                    phone: faker.phone.number(),
+                    birthdate: faker.date.past(),
+                    gender: faker.helpers.arrayElement(['Male', 'Female']),
+                    profile_picture: faker.image.avatar(),
+                    last_login: faker.date.past(),
+                    role: faker.helpers.arrayElement(['Admin', 'User']),
+                    account_status: faker.helpers.arrayElement(['Active', 'Inactive']),
+                    social_media_links: faker.internet.url(),
+                    bio: faker.lorem.paragraph(),
+                    language: faker.helpers.arrayElement(['English', 'Spanish']),
+                    is_blocked: faker.datatype.boolean() ,
+                    blocked_until: faker.date.future(),
+                    deleted_at: null,
+                    created_at: knex.fn.now(),
+                    updated_at: knex.fn.now(),
                 });
             }
             return knex('users').insert(users);
         });
 };
+
