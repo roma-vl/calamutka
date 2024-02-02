fe1adc4169e8e1e070de7687443e7f77cc539641
roma <drakyla60@gmail.com>
2024-02-02 15:19:04 +0200

    add user to seeds

--- api/seeds/users.js
+++ api/seeds/users.js
@@ -3,6 +3,29 @@ export const seed = function (knex) {
     return knex('users').del()
         .then(function () {
             const users = [];
+            users.push({
+                first_name: 'Roma',
+                last_name: 'Volkov',
+                username: faker.internet.userName(),
+                password: 'roma',
+                email: 'Drakyla60@gmail.com',
+                address: faker.location.streetAddress(),
+                phone: faker.phone.number(),
+                birthdate: faker.date.past(),
+                gender: faker.helpers.arrayElement(['Male', 'Female']),
+                profile_picture: faker.image.avatar(),
+                last_login: faker.date.past(),
+                role: faker.helpers.arrayElement(['Admin', 'User']),
+                account_status: faker.helpers.arrayElement(['Active', 'Inactive']),
+                social_media_links: faker.internet.url(),
+                bio: faker.lorem.paragraph(),
+                language: faker.helpers.arrayElement(['English', 'Spanish']),
+                is_blocked: faker.datatype.boolean() ,
+                blocked_until: faker.date.future(),
+                deleted_at: null,
+                created_at: knex.fn.now(),
+                updated_at: knex.fn.now(),
+            });
             for (let i = 0; i < 5; i++) {
                 users.push({
                     first_name: faker.person.firstName(),
--- api/src/modules/users/middleware/validation.js
+++ api/src/modules/users/middleware/validation.js
@@ -1,11 +1,11 @@
 const validateUserData = (req, res, next) => {
-    const { username, password } = req.body;
-    console.log('444444444444444444')
+    const { email, password } = req.body;
+    // console.log('valllll')
 
     // if (username && username.length < 4 ) {
     //     return res.status(400).json({ message: 'Імя має бути довший 6 символів' });
     // }
-    // if (!username || !password) {
+    // if (!email || !password) {
     //     return res.status(400).json({ message: 'Missing required fields' });
     // }
 
