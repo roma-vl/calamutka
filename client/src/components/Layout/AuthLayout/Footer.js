7448c8a99f54abcc75418e53dc4542ce4b7577eb
roma <drakyla60@gmail.com>
2024-02-02 15:13:33 +0200

    layout

--- client/src/components/Layout/AuthLayout/Footer.js
+++ client/src/components/Layout/AuthLayout/Footer.js
@@ -1,8 +1,25 @@
 import {Fragment} from "react";
+import Typography from "@mui/material/Typography";
+import Link from "@mui/material/Link";
+import {BASE_URI} from "../../../constants";
 
+function Copyright(props) {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center" {...props}>
+      {'Copyright © '}
+      <Link color="inherit" href={BASE_URI}>
+        Calamutka
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
 
 const Footer = () => (
-    <Fragment></Fragment>
+    <Fragment>
+      <Copyright sx={{ mt: 8, mb: 4 }} />
+    </Fragment>
 );
 
 export default Footer;
--- client/src/components/Layout/DefaultLayout/Header.js
+++ client/src/components/Layout/DefaultLayout/Header.js
@@ -3,54 +3,82 @@ import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import Link from "@mui/material/Link";
 import Button from "@mui/material/Button";
+import authService from "../../../services/authService";
+import logout from "../../../pages/Auth/Logout";
+import {useNavigate} from "react-router-dom";
 
+const Header = () => {
 
-const Header = () => (
+    const navigate = useNavigate();
 
-            <AppBar
-                position="static"
-                color="default"
-                elevation={0}
-                sx={{ borderBottom: (theme) => `1px solid ${theme.palette.divider}` }}
-            >
-                <Toolbar sx={{ flexWrap: 'wrap' }}>
-                    <Typography variant="h6" color="inherit" noWrap sx={{ flexGrow: 1 }}>
-                        Company name
-                    </Typography>
-                    <nav>
-                        <Link
-                            variant="button"
-                            color="text.primary"
-                            href="/"
-                            sx={{ my: 1, mx: 1.5 }}
-                        >
-                            Home
-                        </Link>
-                        <Link
-                            variant="button"
-                            color="text.primary"
-                            href="pricing"
-                            sx={{ my: 1, mx: 1.5 }}
-                        >
-                            Pricing
-                        </Link>
-                        <Link
-                            variant="button"
-                            color="text.primary"
-                            href="album"
-                            sx={{ my: 1, mx: 1.5 }}
-                        >
-                            Support
-                        </Link>
-                    </nav>
+    const handleLogout = async () => {
+        try {
+            await logout();
+            document.location.reload()
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    return (
+
+      <AppBar
+        position="static"
+        color="default"
+        elevation={0}
+        sx={{ borderBottom: (theme) => `1px solid ${theme.palette.divider}` }}
+      >
+          <Toolbar sx={{ flexWrap: 'wrap' }}>
+              <Typography variant="h6" color="inherit" noWrap sx={{ flexGrow: 1 }}>
+                  Company name
+              </Typography>
+              <nav>
+                  <Link
+                    variant="button"
+                    color="text.primary"
+                    href="/"
+                    sx={{ my: 1, mx: 1.5 }}
+                  >
+                      Home
+                  </Link>
+                  <Link
+                    variant="button"
+                    color="text.primary"
+                    href="pricing"
+                    sx={{ my: 1, mx: 1.5 }}
+                  >
+                      Pricing
+                  </Link>
+                  <Link
+                    variant="button"
+                    color="text.primary"
+                    href="album"
+                    sx={{ my: 1, mx: 1.5 }}
+                  >
+                      Support
+                  </Link>
+              </nav>
+              {authService.isUserLoggedIn() ? (
+                <Button
+                  onClick={handleLogout}
+                  variant="outlined"
+                  sx={{ my: 1, mx: 0.5 }}
+                >
+                    Logout
+                </Button>
+              ) : (
+                <>
                     <Button href="login" variant="outlined" sx={{ my: 1, mx: 0.5 }}>
                         Login
                     </Button>
                     <Button href="register" variant="outlined" sx={{ my: 1, mx: 0.5 }}>
                         Register
                     </Button>
-                </Toolbar>
-            </AppBar>
-);
+                </>
+              )}
+          </Toolbar>
+      </AppBar>
+    )
+};
 
-export default Header;
+export default Header
