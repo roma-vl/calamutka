4ef3e7c92582aa9262e072be524adafea8bfe440
roma <drakyla60@gmail.com>
2024-02-02 15:15:15 +0200

    home add const

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,2 +1,7 @@
 export const USER_KEY = 'chat_app_user'
-export const SERVER_URI = 'http://api.calamutka.com'
+export const HTTP_PROTOCOL = 'http://'
+export const ACCESS_TOKEN = 'accessToken'
+export const BASE_URI = HTTP_PROTOCOL + 'calamutka.com'
+export const SERVER_URI = HTTP_PROTOCOL + 'api.calamutka.com'
+
+
--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,12 +1,38 @@
-
-import { USER_KEY } from 'constants'
+import {USER_KEY} from 'constants'
 import storage from 'utils/storage'
 import Room from "../../components/Room/Room";
 import NameInput from "../../components/NameInput/NameInput";
 import {createTheme} from "@mui/material/styles";
-const defaultTheme = createTheme();
-export default  function Home ()  {
-    const user = storage.get(USER_KEY)
+import {Fragment} from "react";
+import {get} from "../../api/axios.api";
+import authService from "../../services/authService";
 
-    return (user ? <Room /> : <NameInput />);
+const defaultTheme = createTheme();
+export default function Home() {
+  const user = storage.get(USER_KEY)
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const data = await get('/protected');
+    console.log('Received data:', data);
+  };
+
+  return (user ?
+      <Fragment>
+        <Room/>
+        <button onClick={handleSubmit}>Submit</button>
+
+        {authService.isUserLoggedIn() ? (
+         <Fragment>
+           залогінений можна шось показати
+         </Fragment>
+        ) : (
+          <>
+           Гость
+          </>
+        )}
+      </Fragment>
+      :
+      <NameInput/>
+  )
 }
