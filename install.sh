36f015e84fad475280ce90d1a680ddf780a316d9
roma <drakyla60@gmail.com>
2024-02-03 21:47:14 +0200

    nginx Dockerfile

--- install.sh
+++ install.sh
@@ -3,16 +3,24 @@
 # Install npm dependencies
 #sudo rm -rf ./node_modules && npm i
 #
-docker-compose down
-docker-compose up -d
-sleep 20s
+#docker-compose down
+#docker-compose up -d
+#sleep 20s
+
+mkdir -p  ./nginx/ssl
+sudo chmod 777  ./nginx/ssl
+
+
+openssl req -x509 -sha256 -nodes -days 365 -newkey rsa:2048 -keyout ./nginx/ssl/nginx.key -out ./nginx/ssl/nginx.crt
+#openssl req -x509 -sha256 -nodes -newkey rsa:2048 -days 365 -keyout ./build/development/conf/nginx/ssl/localhost.key -out ./build/development/conf/nginx/ssl/localhost.crt
+
 
 # Add containers to /etc/hosts
-if ! [[ $(cat /etc/hosts | grep api_backend) ]]; then
-    sudo -- sh -c -e "echo '127.0.0.1       calamutka.com' >> /etc/hosts";
-    sudo -- sh -c -e "echo '127.0.0.1       api.calamutka.com' >> /etc/hosts";
-    sudo -- sh -c -e "echo '127.0.0.1       admin.calamutka.com' >> /etc/hosts";
-    sudo -- sh -c -e "echo '127.0.0.1       client_frontend' >> /etc/hosts";
-    sudo -- sh -c -e "echo '127.0.0.1       api_backend' >> /etc/hosts";
-    sudo -- sh -c -e "echo '127.0.0.1       client_frontend_admin' >> /etc/hosts";
-fi
+#if ! [[ $(cat /etc/hosts | grep api_backend) ]]; then
+#    sudo -- sh -c -e "echo '127.0.0.1       calamutka.com' >> /etc/hosts";
+#    sudo -- sh -c -e "echo '127.0.0.1       api.calamutka.com' >> /etc/hosts";
+#    sudo -- sh -c -e "echo '127.0.0.1       admin.calamutka.com' >> /etc/hosts";
+#    sudo -- sh -c -e "echo '127.0.0.1       client_frontend' >> /etc/hosts";
+#    sudo -- sh -c -e "echo '127.0.0.1       api_backend' >> /etc/hosts";
+#    sudo -- sh -c -e "echo '127.0.0.1       client_frontend_admin' >> /etc/hosts";
+#fi
