dbc420a30ab82702e4ac423be4728c51d89ea853
roma <drakyla60@gmail.com>
2024-02-07 00:06:07 +0200

    header

--- client/src/components/Layout/DefaultLayout/Header.js
+++ client/src/components/Layout/DefaultLayout/Header.js
@@ -1,25 +1,36 @@
+import React, { useState, useRef } from "react";
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
-import { Link } from "react-router-dom";
 import Button from "@mui/material/Button";
-import authService from "../../../services/authService";
-import logout from "../../../pages/Auth/Logout";
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import IconButton from "@mui/material/IconButton";
 import Menu from "@mui/material/Menu";
 import MenuItem from "@mui/material/MenuItem";
-import { useState } from "react";
+import { Link } from "react-router-dom";
+import authService from "../../../services/authService";
+import logout from "../../../pages/Auth/Logout";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ExitToAppIcon from '@mui/icons-material/ExitToApp';
+import SettingsIcon from '@mui/icons-material/Settings';
+import LoginIcon from '@mui/icons-material/Login';
+import PersonAddIcon from '@mui/icons-material/PersonAdd';
 
 const Header = () => {
     const [anchorEl, setAnchorEl] = useState(null);
+    const [menuOpen, setMenuOpen] = useState(false);
+    const menuRef = useRef(null);
 
     const handleMenuOpen = (event) => {
         setAnchorEl(event.currentTarget);
+        setMenuOpen(true);
     };
 
-    const handleMenuClose = () => {
-        setAnchorEl(null);
+    const handleMenuClose = (event) => {
+        if (!menuRef.current.contains(event.currentTarget)) {
+            setAnchorEl(null);
+            setMenuOpen(false);
+        }
     };
 
     const handleLogout = async () => {
@@ -40,37 +51,75 @@ const Header = () => {
       >
           <Toolbar sx={{ display: 'flex', justifyContent: 'space-between' }}>
               <div style={{ display: 'flex', alignItems: 'center' }}>
-                  <Typography variant="h6" color="inherit" noWrap sx={{ marginRight: '50px' }}>
+                  <Typography variant="h6" color="inherit" noWrap>
                       Company name
                   </Typography>
                   <Button component={Link} to="/" color="inherit">Home</Button>
                   <Button component={Link} to="/pricing" color="inherit">Pricing</Button>
                   <Button component={Link} to="/album" color="inherit">Support</Button>
               </div>
-              <div>
+              <div onMouseLeave={handleMenuClose} ref={menuRef}>
                   <IconButton
-                    onClick={handleMenuOpen}
+                    onMouseEnter={handleMenuOpen}
                     size="large"
                     color="inherit"
                     edge="end"
+                    aria-label="account of current user"
                   >
-                      <AccountCircleIcon />
+                      <AccountCircleIcon
+                        sx={{ fontSize: '32px' }}
+                      />
                   </IconButton>
                   <Menu
                     anchorEl={anchorEl}
-                    open={Boolean(anchorEl)}
+                    open={menuOpen}
                     onClose={handleMenuClose}
+                    anchorOrigin={{
+                        vertical: 'top',
+                        horizontal: 'right',
+                    }}
+                    transformOrigin={{
+                        vertical: 'top',
+                        horizontal: 'right',
+                    }}
+                    PaperProps={{
+                        style: {
+                            marginTop: '50px',
+                            width: 'max-content', // Змінено на 'max-content'
+                            minWidth: '200px', // Додано мінімальну ширину
+                        },
+                    }}
                   >
                       {authService.isUserLoggedIn() ? (
-                        <>
-                            <MenuItem onClick={handleLogout}>Logout</MenuItem>
-                            <MenuItem component={Link} to="/account" onClick={handleMenuClose}>Account Settings</MenuItem>
-                        </>
+                        [
+                            <MenuItem key="logout" onClick={handleLogout}>
+                                <ListItemIcon>
+                                    <ExitToAppIcon fontSize="small" />
+                                </ListItemIcon>
+                                Logout
+                            </MenuItem>,
+                            <MenuItem key="account" component={Link} to="/account" onClick={handleMenuClose}>
+                                <ListItemIcon>
+                                    <SettingsIcon fontSize="small" />
+                                </ListItemIcon>
+                                Account Settings
+                            </MenuItem>
+                        ]
                       ) : (
-                        <>
-                            <MenuItem component={Link} to="/login" onClick={handleMenuClose}>Login</MenuItem>
-                            <MenuItem component={Link} to="/register" onClick={handleMenuClose}>Register</MenuItem>
-                        </>
+                        [
+                            <MenuItem key="login" component={Link} to="/login" onClick={handleMenuClose}>
+                                <ListItemIcon>
+                                    <LoginIcon fontSize="small" />
+                                </ListItemIcon>
+                                Login
+                            </MenuItem>,
+                            <MenuItem key="register" component={Link} to="/register" onClick={handleMenuClose}>
+                                <ListItemIcon>
+                                    <PersonAddIcon fontSize="small" />
+                                </ListItemIcon>
+                                Register
+                            </MenuItem>
+                        ]
                       )}
                   </Menu>
               </div>
