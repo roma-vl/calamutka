ed3e8786d896085e52154bce0124bddb1718c6e5
roma <drakyla60@gmail.com>
2024-02-06 22:15:56 +0200

    menu cabinet

--- client/src/components/Layout/DefaultLayout/Header.js
+++ client/src/components/Layout/DefaultLayout/Header.js
@@ -1,84 +1,82 @@
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
-import Link from "@mui/material/Link";
+import { Link } from "react-router-dom";
 import Button from "@mui/material/Button";
 import authService from "../../../services/authService";
 import logout from "../../../pages/Auth/Logout";
-import {useNavigate} from "react-router-dom";
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import IconButton from "@mui/material/IconButton";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import { useState } from "react";
 
 const Header = () => {
+    const [anchorEl, setAnchorEl] = useState(null);
 
-    const navigate = useNavigate();
+    const handleMenuOpen = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleMenuClose = () => {
+        setAnchorEl(null);
+    };
 
     const handleLogout = async () => {
         try {
             await logout();
-            document.location.reload()
+            document.location.reload();
         } catch (error) {
             console.error(error);
         }
     };
 
     return (
-
       <AppBar
         position="static"
         color="default"
         elevation={0}
         sx={{ borderBottom: (theme) => `1px solid ${theme.palette.divider}` }}
       >
-          <Toolbar sx={{ flexWrap: 'wrap' }}>
-              <Typography variant="h6" color="inherit" noWrap sx={{ flexGrow: 1 }}>
-                  Company name
-              </Typography>
-              <nav>
-                  <Link
-                    variant="button"
-                    color="text.primary"
-                    href="/"
-                    sx={{ my: 1, mx: 1.5 }}
+          <Toolbar sx={{ display: 'flex', justifyContent: 'space-between' }}>
+              <div style={{ display: 'flex', alignItems: 'center' }}>
+                  <Typography variant="h6" color="inherit" noWrap sx={{ marginRight: '50px' }}>
+                      Company name
+                  </Typography>
+                  <Button component={Link} to="/" color="inherit">Home</Button>
+                  <Button component={Link} to="/pricing" color="inherit">Pricing</Button>
+                  <Button component={Link} to="/album" color="inherit">Support</Button>
+              </div>
+              <div>
+                  <IconButton
+                    onClick={handleMenuOpen}
+                    size="large"
+                    color="inherit"
+                    edge="end"
                   >
-                      Home
-                  </Link>
-                  <Link
-                    variant="button"
-                    color="text.primary"
-                    href="pricing"
-                    sx={{ my: 1, mx: 1.5 }}
+                      <AccountCircleIcon />
+                  </IconButton>
+                  <Menu
+                    anchorEl={anchorEl}
+                    open={Boolean(anchorEl)}
+                    onClose={handleMenuClose}
                   >
-                      Pricing
-                  </Link>
-                  <Link
-                    variant="button"
-                    color="text.primary"
-                    href="album"
-                    sx={{ my: 1, mx: 1.5 }}
-                  >
-                      Support
-                  </Link>
-              </nav>
-              {authService.isUserLoggedIn() ? (
-                <Button
-                  onClick={handleLogout}
-                  variant="outlined"
-                  sx={{ my: 1, mx: 0.5 }}
-                >
-                    Logout
-                </Button>
-              ) : (
-                <>
-                    <Button href="login" variant="outlined" sx={{ my: 1, mx: 0.5 }}>
-                        Login
-                    </Button>
-                    <Button href="register" variant="outlined" sx={{ my: 1, mx: 0.5 }}>
-                        Register
-                    </Button>
-                </>
-              )}
+                      {authService.isUserLoggedIn() ? (
+                        <>
+                            <MenuItem onClick={handleLogout}>Logout</MenuItem>
+                            <MenuItem component={Link} to="/account" onClick={handleMenuClose}>Account Settings</MenuItem>
+                        </>
+                      ) : (
+                        <>
+                            <MenuItem component={Link} to="/login" onClick={handleMenuClose}>Login</MenuItem>
+                            <MenuItem component={Link} to="/register" onClick={handleMenuClose}>Register</MenuItem>
+                        </>
+                      )}
+                  </Menu>
+              </div>
           </Toolbar>
       </AppBar>
-    )
+    );
 };
 
-export default Header
+export default Header;
