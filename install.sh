937e6f3c6cbee73e1e287274b1e8c033b6eee9ff
roma <drakyla60@gmail.com>
2024-02-11 22:12:27 +0200

    Додав в інсталятор запуск міграцій і сідів

--- install.sh
+++ install.sh
@@ -1,16 +1,15 @@
-#!/usr/bin/env bash
-
-# Перевірте, чи встановлено програмне забезпечення
-#command -v npm >/dev/null 2>&1 || { echo >&2 "npm не знайдено. Встановіть npm та повторіть спробу."; exit 1; }
-#command -v docker-compose >/dev/null 2>&1 || { echo >&2 "docker-compose не знайдено. Встановіть docker-compose та повторіть спробу."; exit 1; }
-
 # Встановіть npm залежності
-#sudo rm -rf ./node_modules && npm i
+sudo rm -rf ./api/node_modules && npm i
+sudo rm -rf ./client/node_modules && npm i
+sudo rm -rf ./client-admin/node_modules && npm i
 
 # Вимкніть контейнери та підніміть їх
-#docker-compose down
-#docker-compose up -d
-#sleep 20s
+docker-compose down
+docker-compose up -d
+sleep 20s
+
+# Зайти в контейнер api_backend і виконати команди knex
+docker-compose exec api_backend sh -c "npx knex migrate:up && npx knex seed:run"
 
 # Створіть директорію для SSL-сертифікатів
 mkdir -p ./nginx/ssl
@@ -25,7 +24,3 @@ echo '127.0.0.1       calamutka.com api.calamutka.com admin.calamutka.com client
 openssl req -x509 -sha256 -nodes -days 365 -newkey rsa:2048 -keyout ./nginx/ssl/nginx.key -out ./nginx/ssl/nginx.crt -subj "/CN=calamutka.com" -addext "subjectAltName = DNS:api.calamutka.com, DNS:admin.calamutka.com"
 chmod 600 ./nginx/ssl/nginx.crt
 chmod 600 ./nginx/ssl/nginx.key
-
-
-# Отримайте SSL-сертифікати від Let's Encrypt (тестовий режим)
-#certbot certonly --manual --preferred-challenges=http --agree-tos --register-unsafely-without-email -d calamutka.com -d api.calamutka.com -d admin.calamutka.com -d client_frontend -d api_backend -d client_frontend_admin
