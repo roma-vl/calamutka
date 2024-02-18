e03957ca722afcaa0854caaf2ea6bd52162fe92b
roma <drakyla60@gmail.com>
2024-02-18 20:58:41 +0200

    міделвар для корзини

--- client/src/rootReducer.js
+++ client/src/rootReducer.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import cartReducer from './cartReducer';
 import productReducer from "./productReducer";
+import oneProductReducer from "./oneProductReducer";
 
 const rootReducer = combineReducers({
-  cart: cartReducer,
-  product: productReducer
+  cart:       cartReducer,
+  product:    productReducer,
+  oneProduct: oneProductReducer
 });
 
 export default rootReducer;
--- client/src/store.js
+++ client/src/store.js
@@ -1,6 +1,7 @@
-import { createStore } from 'redux';
+import {applyMiddleware, createStore} from 'redux';
 import rootReducer from './rootReducer.js';
+import localstorageMiddleware from "./localstorageMiddleware";
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(localstorageMiddleware));
 
 export default store;
