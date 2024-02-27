71ae6fc2ebc868642689e839f9a7461d183c7761
roma <drakyla60@gmail.com>
2024-02-27 21:04:00 +0200

    UserList переписав дизайн компонента

--- client/src/components/Room/UserList/UserList.js
+++ client/src/components/Room/UserList/UserList.js
@@ -1,28 +1,55 @@
-import { AiOutlineUser } from 'react-icons/ai'
+import { useState } from 'react';
+import { AiOutlineUser } from 'react-icons/ai';
+import { Avatar, List, ListItem, ListItemAvatar, ListItemText, Typography } from '@mui/material';
+import { makeStyles } from '@mui/styles';
+
+const useStyles = makeStyles((theme) => ({
+  listItem: {
+    '&:hover': {
+      backgroundColor: theme.palette.primary.light,
+      cursor: 'pointer',
+    },
+    '&:active': {
+      backgroundColor: theme.palette.primary.dark,
+    },
+  },
+  activeListItem: {
+    backgroundColor: theme.palette.primary.main,
+    '&:hover': {
+      backgroundColor: theme.palette.primary.main,
+    },
+  },
+}));
 
 export default function UserList({ users }) {
+  const classes = useStyles();
+  const [activeUserId, setActiveUserId] = useState(null);
 
-    const userMap = new Map();
+  const handleListItemClick = (userId) => {
+    setActiveUserId(userId === activeUserId ? null : userId);
+  };
 
-    const filteredUsers = users.filter((user) => {
-        const alreadyExists = userMap.has(user.userId);
-        if (!alreadyExists) {
-            userMap.set(user.userId, true);
-        }
-        return !alreadyExists;
-    });
-
-    return (
-        <div className='container user'>
-            <h2>Users</h2>
-            <ul className='list user'>
-                {filteredUsers.map(({ userId, userName }) => (
-                    <li key={userId} className='item user'>
-                        <AiOutlineUser className='icon user' />
-                        {userName}
-                    </li>
-                ))}
-            </ul>
-        </div>
-    )
+  return (
+    <div className='container user'>
+      <Typography variant="h5" gutterBottom component="div">
+        Users
+      </Typography>
+      <List>
+        {users.map(({ userId, userName }) => (
+          <ListItem
+            key={userId}
+            className={`${classes.listItem} ${userId === activeUserId ? classes.activeListItem : ''}`}
+            onClick={() => handleListItemClick(userId)}
+          >
+            <ListItemAvatar>
+              <Avatar>
+                <AiOutlineUser />
+              </Avatar>
+            </ListItemAvatar>
+            <ListItemText primary={userName} />
+          </ListItem>
+        ))}
+      </List>
+    </div>
+  )
 }
