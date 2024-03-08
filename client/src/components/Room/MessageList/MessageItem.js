98719f5ef436af48fefc92f3c9df5161b46f68ce
roma <drakyla60@gmail.com>
2024-03-08 20:03:30 +0200

    правки по повідомленях

--- client/src/components/Room/MessageList/MessageItem.js
+++ client/src/components/Room/MessageList/MessageItem.js
@@ -1,26 +1,25 @@
-import { CgTrashEmpty } from 'react-icons/cg';
-import { GiSpeaker } from 'react-icons/gi';
-import { useSpeechSynthesis } from 'react-speech-kit';
+import {CgTrashEmpty} from 'react-icons/cg';
+import {GiSpeaker} from 'react-icons/gi';
+import {useSpeechSynthesis} from 'react-speech-kit';
 import TimeAgo from 'react-timeago';
-import { SERVER_URI, USER_KEY } from 'constants.js';
-import storage from 'utils/storage';
+import {SERVER_URI} from 'constants.js';
 import './MessageItem.css';
 import CustomAudioPlayer from "./Audio";
 import CustomVideoPlayer from "./Video";
 import IconButton from "@mui/material/IconButton";
-import { useSelector } from "react-redux";
+import {useSelector} from "react-redux";
 
-export default function MessageItem({ message, removeMessage }) {
+export default function MessageItem({message, removeMessage}) {
   const user = useSelector(state => state.user.userData);
-  const { speak, voices } = useSpeechSynthesis();
+  const {speak, voices} = useSpeechSynthesis();
   const lang = document.documentElement.lang || 'en';
   const voice = voices.find((v) => v.lang.includes(lang) && v.name.includes('Google'));
 
-  const { messageType, textOrPathToFile } = message;
+  const {messageType, textOrPathToFile} = message;
   const pathToFile = `${SERVER_URI}/files${textOrPathToFile}`;
 
   let element;
-  let isMyMessage; // Оголошуємо змінну isMyMessage в області видимості компоненту
+  let isMyMessage;
 
   switch (messageType) {
     case 'text':
@@ -28,16 +27,16 @@ export default function MessageItem({ message, removeMessage }) {
         <>
           <IconButton
             className='btn btn_speak'
-            onClick={() => speak({ text: textOrPathToFile, voice })}
+            onClick={() => speak({text: textOrPathToFile, voice})}
           >
-            <GiSpeaker className='icon speak' />
+            <GiSpeaker className='icon speak'/>
           </IconButton>
           <p>{textOrPathToFile}</p>
         </>
       );
       break;
     case 'image':
-      element = <img src={pathToFile} alt='' width="100%" />;
+      element = <img src={pathToFile} alt='' width="100%"/>;
       break;
     case 'audio':
       element = <CustomAudioPlayer src={pathToFile}></CustomAudioPlayer>;
@@ -49,24 +48,24 @@ export default function MessageItem({ message, removeMessage }) {
       return null;
   }
 
-  if (user) {isMyMessage = user.id == message.userId;}
-
+  if (user) {
+    isMyMessage = user.id == message.userId;
+  }
   return user && (
     <li className={`item message ${isMyMessage ? 'my' : ''}`}>
-      <span className='username'>{isMyMessage ? 'Me' : message.userName}</span>
-
+      <span className='username'>{isMyMessage ? 'Me' : message.authorName}</span>
       <div className='inner'>
         {element}
 
         {isMyMessage && (
           <IconButton className='btn btn_remove ' onClick={() => removeMessage(message)}>
-            <CgTrashEmpty className='icon remove' />
+            <CgTrashEmpty className='icon remove'/>
           </IconButton>
         )}
       </div>
 
       <p className='datetime'>
-        <TimeAgo date={message.created_at} />
+        <TimeAgo date={message.created_at}/>
       </p>
     </li>
   );
--- client/src/components/Room/MessageList/MessageList.js
+++ client/src/components/Room/MessageList/MessageList.js
@@ -1,4 +1,4 @@
-import {Fragment} from 'react';
+import {Fragment, useEffect} from 'react';
 import MessageItem from './MessageItem';
 import {Paper} from '@mui/material';
 
@@ -6,13 +6,14 @@ export default function MessageList({log, messages, removeMessage}) {
   console.log(log)
   return (
     <Fragment>
+      <div>онлайн/офлайн {log}</div>
       {messages && messages.slice().reverse().map((message, index) => (
 
         <Paper key={index} elevation={1} sx={{
           padding: 1,
           borderRadius: 1,
           minWidth: '90%',
-          maxWidth: '90px',
+          // maxWidth: '90px',
           alignSelf: message.id % 2 === 1 ? 'flex-end' : 'flex-start',
           backgroundColor: message.id % 2 === 1 ? 'primary.light' : 'background.paper',
           color: message.id % 2 === 1 ? 'white' : 'text.primary',
@@ -21,7 +22,7 @@ export default function MessageList({log, messages, removeMessage}) {
           marginBottom: 1
         }}>
           <MessageItem
-            key={message.messageId}
+            key={message.id}
             message={message}
             removeMessage={removeMessage}
           />
