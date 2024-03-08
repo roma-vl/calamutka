e35748dba74b0d5f16eeceafbc266edd46291d5e
roma <drakyla60@gmail.com>
2024-03-08 20:08:14 +0200

    методи для пошуку кімнат

--- api/src/modules/messages/repositories/RoomsRepository.js
+++ api/src/modules/messages/repositories/RoomsRepository.js
@@ -4,8 +4,20 @@ class RoomsRepository {
     return await Rooms.query().insert(room);
   }
 
+  static async findRoomByUsers(userFrom, userTo) {
+    return await Rooms.query()
+      .where(builder => {
+        builder.where('user_from', userFrom).andWhere('user_to', userTo)
+          .orWhere('user_from', userTo).andWhere('user_to', userFrom);
+      })
+      .first();
+  }
+
+  static async findRoomByUserId(userId) {
+    return await Rooms.query().where('user_from', userId).orWhere('user_to', userId);
+  }
+
   static async getByRoomId(roomId) {
-    console.log(roomId);
     return await Rooms.query().where('id', roomId);
   }
 
