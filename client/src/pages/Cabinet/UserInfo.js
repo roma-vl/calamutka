cec26f52c320ff95c04d8c63bd254b19f4a9f1f3
roma <drakyla60@gmail.com>
2024-10-09 15:04:17 +0300

    edit avatar

--- client/src/pages/Cabinet/UserInfo.js
+++ client/src/pages/Cabinet/UserInfo.js
@@ -1,12 +1,15 @@
-import React, { Fragment, useState } from 'react';
+import { Fragment, useState, useRef } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Avatar from '@material-ui/core/Avatar';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import EditIcon from '@material-ui/icons/Edit';
+import { Grid, Dialog, DialogActions, DialogContent, DialogTitle } from "@material-ui/core";
+import AvatarEditor from 'react-avatar-editor';
 import { toast, ToastContainer } from 'react-toastify';
-import { Grid } from "@material-ui/core";
+import Dropzone from 'react-dropzone';
+import { Link } from "react-router-dom";
 
 const useStyles = makeStyles((theme) => ({
   paper: {
@@ -19,7 +22,7 @@ const useStyles = makeStyles((theme) => ({
     width: theme.spacing(25),
     height: theme.spacing(25),
     margin: 'auto',
-    marginBottom: 40
+    marginBottom: 40,
   },
   userInfo: {
     textAlign: 'left',
@@ -31,18 +34,81 @@ const useStyles = makeStyles((theme) => ({
   infoItem: {
     marginBottom: theme.spacing(1),
   },
+  dialogContent: {
+    textAlign: 'center',
+  },
 }));
 
-const UserInfo = ({user}) => {
+const UserInfo = ({ user }) => {
   const classes = useStyles();
+  const [openModal, setOpenModal] = useState(false);
+  const [image, setImage] = useState(user.profile_picture);
+  const editorRef = useRef(null);
   const [isEditing, setIsEditing] = useState(false);
   const notify = () => toast("Wow so easy!");
+  const [state, setState] = useState({
+    scale: 1,
+    rotate: 0,
+    width: 300,
+    height: 300,
+    allowZoomOut: false,
+    isTransparent: false,
+    border: 0,
+  });
+
+  // const handleOpenModal = () => setOpenModal(true);
 
   const handleEditClick = () => {
     setIsEditing(!isEditing);
+  }
+  const handleCloseModal = () => setOpenModal(false);
+  const handleDrop = (droppedFiles) => setImage(droppedFiles[0]);
+
+  const urlToBase64 = async (url) => {
+    const response = await fetch(url);
+    const blob = await response.blob();
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.onloadend = () => resolve(reader.result);
+      reader.onerror = reject;
+      reader.readAsDataURL(blob);
+    });
   };
 
 
+  const handleOpenModal = async () => {
+    if (typeof user.profile_picture === 'string') {
+      const base64Image = await urlToBase64(user.profile_picture);
+      setImage(base64Image); // Передаємо Base64 в AvatarEditor
+    }
+    setOpenModal(true);
+  };
+
+
+  const handleScaleChange = (e) => setState({ ...state, scale: parseFloat(e.target.value) });
+  const handleRotateLeft = () => setState({ ...state, rotate: (state.rotate - 90) % 360 });
+  const handleRotateRight = () => setState({ ...state, rotate: (state.rotate + 90) % 360 });
+
+  const handleSave = () => {
+    const img = editorRef.current.getImageScaledToCanvas().toDataURL("image/png");
+    console.log("Saved Image: ", img);
+
+    // Створюємо посилання для завантаження
+    const link = document.createElement("a");
+    link.href = img; // Вставляємо Base64 зображення
+    link.download = "avatar.png"; // Ім'я файлу при завантаженні
+
+    // link.click();
+    toast.success("Success Notification !", {
+      position: "top-right"
+    });
+    setOpenModal(false);
+  };
+
+  // const handleTabClick = (tab) => {
+  //   setActiveTab(tab);
+  //   window.location.href = `/cabinet#${tab}`;
+  // };
 
   return (
     <Fragment>
@@ -53,9 +119,9 @@ const UserInfo = ({user}) => {
             <Button
               variant="outlined"
               color="primary"
-              startIcon={<EditIcon/>}
+              startIcon={<EditIcon />}
               className={classes.editIcon}
-              onClick={handleEditClick}
+              onClick={handleOpenModal}
             >
               Додати фото
             </Button>
@@ -98,24 +164,61 @@ const UserInfo = ({user}) => {
               <div className={classes.infoItem}>
                 <Typography variant="subtitle1">Роль: {user.role}</Typography>
               </div>
+
               <Button
                 variant="outlined"
                 color="primary"
                 startIcon={<EditIcon/>}
                 className={classes.editIcon}
-                onClick={handleEditClick}
-                onClick={notify}
-              >
-                Редагувати профіль
-              </Button>
-              <div>
-                <Button onClick={notify}>Notify!</Button>
-                <ToastContainer/>
-              </div>
+                component={Link}
+                to="/cabinet#edit"
+                onClick={() => handleTabClick('edit')}
+              >Редагувати профіль </Button>
+
             </div>
           </Paper>
         </Grid>
       </Grid>
+
+      <Dialog open={openModal} onClose={handleCloseModal} aria-labelledby="form-dialog-title">
+        <DialogTitle id="form-dialog-title">Вибір фото профілю</DialogTitle>
+        <DialogContent className={classes.dialogContent}>
+          <Dropzone onDrop={handleDrop} noClick noKeyboard>
+            {({ getRootProps, getInputProps }) => (
+              <div {...getRootProps()}>
+                <AvatarEditor
+                  ref={editorRef}
+                  image={image}
+                  scale={state.scale}
+                  width={state.width}
+                  height={state.height}
+                  rotate={state.rotate}
+                />
+                <input {...getInputProps()} />
+              </div>
+            )}
+          </Dropzone>
+          <div>
+            Zoom:
+            <input
+              type="range"
+              min={state.allowZoomOut ? '0.1' : '1'}
+              max="2"
+              step="0.01"
+              value={state.scale}
+              onChange={handleScaleChange}
+            />
+            <br />
+            Rotate:
+            <button onClick={handleRotateLeft}>Left</button>
+            <button onClick={handleRotateRight}>Right</button>
+          </div>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleCloseModal} color="primary">Закрити</Button>
+          <Button onClick={handleSave} color="primary">Зберегти</Button>
+        </DialogActions>
+      </Dialog>
     </Fragment>
   );
 };
